eb4ff16a3c35ce00a4d2108dadb509927cbc2d96
Alexandr <Rem271@mail.ru>
2024-04-22 15:06:05 +0300

    4. Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkParseIsEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("is empty");
+    }
+
+    @Test
+    void checkParse() {
+        NameLoad nameLoad = new NameLoad();
+        nameLoad.parse("key=value");
+        Map<String, String> result = new HashMap<>();
+        result.put("key", "value");
+        assertThat(result).isEqualTo(nameLoad.getMap());
+    }
+}
