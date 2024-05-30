9a84879586b8bf998ff60ad242a7bbd0d106543f
Alexandr <Rem271@mail.ru>
2024-05-30 10:40:12 +0300

    lllll

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,13 +1,21 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static void main(String[] args) {
-        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
-            output.write("Hello, world!".getBytes());
-            output.write(System.lineSeparator().getBytes());
+        try (PrintWriter output = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
+            output.println("Hello, world!");
+            output.printf("%s%n", "Some string");
+            output.printf("%d%n", 10);
+            output.printf("%f%n", 1.5f);
+            output.printf("%s%d%f%n", "Puk", 35, 5.8f);
         } catch (IOException e) {
             e.printStackTrace();
         }
