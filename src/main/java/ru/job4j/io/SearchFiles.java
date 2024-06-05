4e7dbbcabe4e506a338f22583cf33d7b5b285eec
Alexandr <Rem271@mail.ru>
2024-06-05 12:31:53 +0300

    10. Сканирование файловой системы [#106929 #499230]

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -29,7 +29,7 @@ public class SearchFiles implements FileVisitor<Path> {
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        if (condition.test(file) && file.getFileName().toString().endsWith(".js")) {
+        if (condition.test(file)) {
             paths.add(file);
         }
         return FileVisitResult.CONTINUE;
