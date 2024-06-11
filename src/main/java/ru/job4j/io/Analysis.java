d2c868dedac98a2de5161c2db72aa98f288e026c
Alexandr <Rem271@mail.ru>
2024-06-11 16:36:31 +0300

    7. Анализ доступности сервера [#859 #498823]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,45 +1,27 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
     public void unavailable(String source, String target) {
-        List<String> lineEntries = new ArrayList<>();
-        List<String> timePeriods = new ArrayList<>();
-
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(source));
+             PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(target)))) {
             String readLine;
+            boolean flag = true;
             while ((readLine = bufferedReader.readLine()) != null) {
-                lineEntries.add(readLine);
+                String[] split = readLine.split(" ");
+                int status = Integer.parseInt(split[0]);
+                String time = split[1];
+                if ((status == 400 || status == 500) && flag) {
+                    flag = false;
+                    printWriter.print(String.format("%s;", time));
+                } else if ((status == 200 || status == 300) && !flag) {
+                    flag = true;
+                    printWriter.print(String.format("%s;%s", time, System.lineSeparator()));
+                }
             }
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        String start = null;
-        String end;
-        for (String line : lineEntries) {
-            String[] split = line.split(" ");
-            int status = Integer.parseInt(split[0]);
-            String time = split[1];
-            if ((status == 400 || status == 500) && start == null) {
-                start = time;
-            } else if ((status == 200 || status == 300) && start != null) {
-                end = time;
-                timePeriods.add(start + ";" + end + ";");
-                start = null;
-            }
-        }
-
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(target))) {
-            for (String strings : timePeriods) {
-                bufferedWriter.write(strings);
-                bufferedWriter.newLine();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
