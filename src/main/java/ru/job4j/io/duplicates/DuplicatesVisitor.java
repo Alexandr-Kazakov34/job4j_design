d27bd0b73616c78199b624c07f34d1f410e2c80e
Alexandr <Rem271@mail.ru>
2024-06-12 12:52:45 +0300

    11. Поиск дубликатов [#315066 #499239]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -2,28 +2,35 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<FileProperty, Path> filePropertyPathMap = new HashMap<>();
+
+
+    private final Map<FileProperty, List<Path>> duplicates = new HashMap<>();
 
     @Override
-    public FileVisitResult visitFile(Path file,
-                                     BasicFileAttributes attributes) throws IOException {
-        FileProperty fileProperty = new FileProperty(attributes.size(), file.getFileName().toString());
-        if (!filePropertyPathMap.containsKey(fileProperty)) {
-            filePropertyPathMap.put(fileProperty, file);
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) throws IOException {
+        FileProperty firstFile = new FileProperty(Files.size(file), file.getFileName().toString());
+        if (!duplicates.containsKey(firstFile)) {
+            ArrayList<Path> paths = new ArrayList<>(List.of(file));
+            duplicates.put(firstFile, paths);
+        } else {
+            duplicates.get(firstFile).add(file);
         }
         return super.visitFile(file, attributes);
     }
 
-    public void printFilePropertyPathMap() {
-        for (Map.Entry<FileProperty, Path> entry : filePropertyPathMap.entrySet()) {
+    public void printDuplicates() {
+        for (Map.Entry<FileProperty, List<Path>> entry : duplicates.entrySet()) {
+            if (entry.getValue().size() > 1) {
                 for (Path file : entry.getValue()) {
                     System.out.println(file);
+                }
             }
         }
     }
